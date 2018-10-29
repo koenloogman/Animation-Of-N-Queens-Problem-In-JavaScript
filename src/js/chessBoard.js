dedee580a2fcd7fa530ff8a722a367d673369304
Koen Loogman <koen.loogman@atos.net>
2018-10-29 16:38:07 +0100

    scaleable chessboard

--- src/js/chessBoard.js
+++ src/js/chessBoard.js
@@ -1,92 +1,117 @@
 'use strict';
-const Tile = require('./tile');
 
-function toAlphabet(i, alphabet, size = 1) {
-    var alphabet = alphabet.split('');
-    var base = alphabet.length;
+const Two = require('two.js');
 
-    var result = "";
-    while (i >= base) {
-        result = alphabet[i % base] + result;
-        i = Math.floor(i / base);
-    }
-    result = alphabet[i % base] + result;
-    while (result.length < size) {
-        result = alphabet[0] + result;
-    }
-    return result;
-}
 class ChessBoard {
-    constructor(size, n = 8) {
-        /**
-         * @type {Number}
-         */
-        this.cols = n;
 
-        /**
-         * @type {Number}
-         */
-        this.rows = n;
+    constructor(two, n = 8, size = 500) {
+        this.two = two;
 
-        /**
-         * @type {Array<Tile>}
-         */
-        this.tiles = new Array();
-        for (var i = 0; i < this.cols * this.rows; i++) {
-            this.tiles.push(new Tile(this));
+        // parameters
+        this._noStroke = false;
+        this._whiteFill = "#fff";
+        this._blackFill = "#000";
+
+        // chess board
+        this.board = new Two.RoundedRectangle(0, 0, 0, 0, 0);
+        this.group = new Two.Group(this.board);
+        this.tiles = [];
+        this.size = size;
+        this.n = n;
+
+        two.add(this.group);
+    }
+
+    get n() {
+        return this._n;
+    }
+
+    set n(n) {
+        this._n = n;
+        this.group.remove(this.tiles);
+        this.group.add(this.board);
+        this.tiles = [];
+
+        // create tiles
+        for (var i = 0; i < n * n; i++) {
+            let x = i % n;
+            let y = Math.floor(i / n);
+
+            let tile = new Two.RoundedRectangle(0, 0, 0, 0);
+            this.tiles.push(tile);
+            this.group.add(tile);
+            
+            // TODO replace colors
+            tile.fill = x % 2 == y % 2 ? this._whiteFill : this._blackFill;
         }
+        // resize tiles to fit the board
+        this.size = this.size;
+    }
 
-        /**
-         * @type {Array<String>}
-         */
-        this.letters = new Array();
-        let base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        let length = Math.log(n) / Math.log(base.length);
-        for (var i = 0; i < n; i++) {
-            this.letters.push(toAlphabet(i, base, length));
+    get size() {
+        return this._size;
+    }
+
+    set size(size) {
+        this._size = size;
+
+        // rearrange group and board
+        this.board.width = size;
+        this.board.height = size;
+        this.board.radius = size * 0.0125;
+
+        // define sizes
+        let innerSize = size * (1 - 0.035 * 8 / this.n);
+        let margin = size * 0.01 * (this.n > 1 ? 8 / this.n : 0);
+        let tileSize = (innerSize - (this.n - 1) * margin) / this.n;
+
+        // resize tiles
+        this.tiles.forEach((tile, i) => {
+            let x = i % this.n;
+            let y = Math.floor(i / this.n);
+
+            tile.height = tileSize;
+            tile.width = tileSize;
+            tile.radius = tileSize * 0.05;
+            tile.translation.set(
+                x * (tileSize + margin) + tileSize / 2 - innerSize / 2,
+                y * (tileSize + margin) + tileSize / 2 - innerSize / 2
+            );
+        });
+        if (this._noStroke) {
+            this.noStroke();
         }
     }
 
-    /**
-     * @param {Tile} tile 
-     */
-    getTilePosition(tile) {
-        var index = this.tiles.indexOf(tile);
-        return {'x': index % this.cols, 'y': Math.floor(index / this.cols)}
+    whiteTiles() {
+        return this.tiles.filter((tile, i) => (i % this.n) % 2 == Math.floor(i / this.n) % 2);
+    }
+    
+    blackTiles() {
+        return this.tiles.filter((tile, i) => (i % this.n) % 2 != Math.floor(i / this.n) % 2);
     }
 
-    /**
-     * @param {Number} x 
-     * @param {Number} y 
-     */
-    getTileByPosition(x, y) {
-        return this.tiles[x + y * this.cols];
+    get whiteFill() {
+        return this._whiteFill;
     }
 
-    /**
-     * @param {Array<Array<String>>} arr 
-     */
-    setQueens(arr) {
-        /**
-         * @type {Array<String>}
-         */
-        var units = arr.filter(a => a.length == 1).map(a => a[0]);
-        /**
-         * @param {Array<Array<Number>>}
-         */
-        var queens = units.filter(a => a.substr(0, 1) != '!').map(a => a.split(',').map(b => Number.parseInt(b) - 1));
-        var notQueens = units.filter(a => a.substr(0, 1) == '!').map(a => a.substr(1).split(',').map(b => Number.parseInt(b) - 1));
+    set whiteFill(color) {
+        this._whiteFill = color;
+        this.whiteTiles().forEach((tile) => tile.fill = color);
+    }
 
-        this.tiles.forEach(tile => {
-            tile.queen = false;
-            tile.mark = false;
-        });
-        queens.forEach(queen => {
-            this.getTileByPosition(queen[0], queen[1]).queen = true;
-        });
-        notQueens.forEach(notQueen => {
-            this.getTileByPosition(notQueen[0], notQueen[1]).mark = true;
-        });
+    get blackFill() {
+        return this._blackFill;
+    }
+
+    set blackFill(color) {
+        this._blackFill = color;
+        this.blackTiles().forEach((tile) => tile.fill = color);
+    }
+
+    noStroke() {
+        this._noStroke = true;
+        this.group.noStroke();
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 'use strict';
+
 const $ = require('jquery');
 const fs = require('fs');
 const Two = require('two.js');
+const ChessBoard = require('./chessBoard.js');
 
 //Force page refresh on hot reload
 if (module.hot) {
@@ -10,88 +12,50 @@ if (module.hot) {
     })
 }
 
-let two = new Two({
-    fullscreen: true
-}).appendTo(document.body);
-two.scene.translation.set(two.width / 2, two.height / 2);
-
-// define sizes
-let n = 8;
-let size = Math.min(two.width, two.height) * 0.8;
-let outerMargin = size * 0.035 * 8 / n;
-let innerMargin = size * 0.01 * (n > 1 ? 8 / n : 0);
-let innerSize = size - 2 * outerMargin;
-let tileSize = (innerSize - (n - 1) * innerMargin) / n;
-
 // define colors
 let colors = {
     'black': "#353238",
     'white': "#DED9D4",
-    'brown': "#73563F",
-    'gray': "#888888"
+    'wood': "#73563F",
+    'queen': "#888888"
 };
 
-// create board
-let board = two.makeRoundedRectangle(size / 2, size / 2, size, size, size * 0.0125);
-board.fill = colors.brown;
+let two = new Two({
+    fullscreen: true,
+    autostart: true
+}).appendTo(document.body);
 
-let chessBoard = two.makeGroup(board);
-chessBoard.translation.set(- size / 2, -size / 2);
+let chessBoard, state = startingState, n = 8, ln = 8;
 
-// get queen
-$('body').append(fs.readFileSync('./src/svg/queen.svg', 'utf8').toString());
-let firstQueen = two.interpret($('#queen')[0]).center(); // 400px scale
-firstQueen.scale *= tileSize / 400;
-firstQueen.fill = colors.gray;
-$('#queen').remove();
+function startingState() {
+    let nextState = idleState;
 
-// add tiles
-let tiles = new Array();
-let queens = new Array();
-for (let i = 0; i < n * n; i++) {
-    let x = i % n;
-    let y = Math.floor(i / n);
-    let left = (tileSize + innerMargin) * x + tileSize * 0.5 + outerMargin;
-    let top = (tileSize + innerMargin) * y + tileSize * 0.5 + outerMargin;
+    two.clear();
+    chessBoard = new ChessBoard(two, n, Math.min(two.width - 50, two.height - 50));
+    chessBoard.board.fill = colors.wood;
+    chessBoard.whiteFill = colors.white;
+    chessBoard.blackFill = colors.black;
+    chessBoard.noStroke();
 
-    let tile = two.makeRoundedRectangle(0, 0, tileSize, tileSize, tileSize * 0.05);
-    tile.fill = x % 2 == y % 2 ? colors.white : colors.black;
-
-    let group = two.makeGroup(tile);
-    group.translation.set(left, top);
-    chessBoard.add(group);
-
-    tiles.push(group);
-}
-chessBoard.noStroke();
-
-for (let i = 0; i < n; i++) {
-    let queen = i == 0 ? firstQueen : firstQueen.clone();
-    queens.push(queen);
-    two.remove(queen);
+    two.scene.translation.set(two.width / 2, two.height / 2);
+    console.log(chessBoard);
+    return nextState;
 }
 
+function idleState() {
+    let max = 3200;
+    n = ++n % max;
+    let nn = Math.round(Math.abs(n - max / 2) / 100);
+    if (ln != nn) {
+        chessBoard.n = nn;
+    }
+    ln = chessBoard.n;
+    return idleState;
+}
 
-two.bind('update', function(frameCount) {
-    //queens.forEach(queen => queen.rotation += Math.PI / 258);
-}).bind('resize', function() {
-    chessBoard.translation.set(two.width / 2, two.height / 2);
-}).play();
-
-// events
-two.update();
-// hide queens
-tiles.forEach((tile, index) => {
-    $(tile._renderer.elem)
-        .css('cursor', 'pointer')
-        .click(function(e) {
-            let i = Math.floor(index / n);
-            let queen = queens[i];
-            queen.translation.set(0, 0);
-            if (queen.parent == tile) {
-                tile.remove(queen);
-            } else {
-                tile.add(queen);
-            }
-        });
+two.bind('update', (frame, td) => {
+    state = state();
+}).bind('resize', (frame, td) => {
+    two.scene.translation.set(two.width / 2, two.height / 2);
+    chessBoard.size = Math.min(two.width - 50, two.height - 50);
 });
